c3195e6b4edc0d2bc753b5714155832e1d46ab9b
LinusL612 <Linus.Lundh123@gmail.com>
2026-02-24 17:42:02 +0100

    Add files via upload

---
+++ upgift till max/upgift till max/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace upgift_till_max
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.updbtn = new System.Windows.Forms.Button();
+            this.Penistimer = new System.Windows.Forms.Timer(this.components);
+            this.scorebord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // updbtn
+            // 
+            this.updbtn.BackColor = System.Drawing.Color.Red;
+            this.updbtn.Location = new System.Drawing.Point(-12, 402);
+            this.updbtn.Name = "updbtn";
+            this.updbtn.Size = new System.Drawing.Size(827, 52);
+            this.updbtn.TabIndex = 0;
+            this.updbtn.Text = "KILL YOUR SELF";
+            this.updbtn.UseVisualStyleBackColor = false;
+            this.updbtn.Click += new System.EventHandler(this.updbtn_Click);
+            // 
+            // Penistimer
+            // 
+            this.Penistimer.Enabled = true;
+            this.Penistimer.Interval = 75;
+            this.Penistimer.Tick += new System.EventHandler(this.Penistimer_Tick);
+            // 
+            // scorebord
+            // 
+            this.scorebord.AutoSize = true;
+            this.scorebord.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.scorebord.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorebord.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.scorebord.Location = new System.Drawing.Point(690, 9);
+            this.scorebord.Name = "scorebord";
+            this.scorebord.Size = new System.Drawing.Size(102, 26);
+            this.scorebord.TabIndex = 1;
+            this.scorebord.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scorebord);
+            this.Controls.Add(this.updbtn);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updbtn;
+        private System.Windows.Forms.Timer Penistimer;
+        private System.Windows.Forms.Label scorebord;
+    }
+}
+
---
+++ upgift till max/upgift till max/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace upgift_till_max
+{
+    public partial class Form1 : Form
+    {
+        List<PointF> snare = new List<PointF>(); // list att spara alla delar av ormen
+        int score = 0; // variabel för att hålla koll på poängen
+        string Direction = "right"; // variabel för att hålla koll på riktningen
+        float MoveSpeed = 15; // variabel för att bestämma hur snabbt ormen rör sig
+        float x = 300; // startposition för ormen i x axeln
+        float y = 120; // startposition för ormen i y axeln
+        float Foodsize = 15; // variabel för att bestämma storleken på maten
+        int Foodx = 180; // startposition för maten i x axeln
+        int Foody = 120; // startposition för maten i y axeln
+        Random rand = new Random(); // en random generator för att slumpa fram positionen på maten
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += form1_KeyDown;
+            InitSnake(); // en funktion som skapar ormen med startlängd 4
+        }
+
+        private void InitSnake() // skapar ormen med startlängd 4
+        {
+            snare.Clear();
+            snare.Add(new PointF(x - MoveSpeed, y));
+            snare.Add(new PointF(x - MoveSpeed * 2, y));
+            snare.Add(new PointF(x - MoveSpeed * 3, y));
+        }
+
+        protected override void OnPaint(PaintEventArgs draw)
+        {
+            draw.Graphics.FillEllipse(new SolidBrush(Color.DarkViolet), x, y, MoveSpeed, MoveSpeed);
+            draw.Graphics.FillEllipse(new SolidBrush(Color.Red), Foodx, Foody, Foodsize, Foodsize);
+
+            foreach (PointF part in snare) // loop för att rita ut alla delar av ormen
+            {
+                draw.Graphics.FillEllipse(new SolidBrush(Color.DarkViolet), part.X, part.Y, MoveSpeed, MoveSpeed);
+            }
+        }
+
+        private void form1_KeyDown(object sender, KeyEventArgs e) // en funktion för alla knappar som styr ormen
+        {
+            if (e.KeyCode == Keys.W && Direction != "down")
+                Direction = "up";
+
+            if (e.KeyCode == Keys.S && Direction != "up")
+                Direction = "down";
+
+            if (e.KeyCode == Keys.A && Direction != "right")
+                Direction = "left";
+
+            if (e.KeyCode == Keys.D && Direction != "left")
+                Direction = "right";
+        }
+
+        public void Death() // en funktion som kollar om ormen är i sig själv eller utanför gränserna och då dör den
+        {
+            if (x < 0 || x > 570 || y < 0 || y > 320)
+            {
+                ResetGame();
+                return;
+            }
+
+            foreach (PointF part in snare)
+            {
+                if (part.X == x && part.Y == y)
+                {
+                    ResetGame();
+                    return;
+                }
+            }
+        }
+
+        private void ResetGame() // startar om spelet och nollar alla variabler
+        {
+            Penistimer.Stop();
+            MessageBox.Show("You died");
+            x = 300;
+            y = 120;
+            score = 0;
+            scorebord.Text = "Score: 0";
+            Direction = "right";
+            InitSnake();
+            Penistimer.Start();
+        }
+
+        private void Penistimer_Tick(object sender, EventArgs e) // en timer :)
+        {
+            snare.Insert(0, new PointF(x, y));
+
+            switch (Direction) // en switch som ändrar hur ormen rör sig beroende på vilken riktning den har
+            {
+                case "up":
+                    y -= MoveSpeed;
+                    break;
+                case "down":
+                    y += MoveSpeed;
+                    break;
+                case "left":
+                    x -= MoveSpeed;
+                    break;
+                case "right":
+                    x += MoveSpeed;
+                    break;
+            }
+
+            if (Foody == y && Foodx == x) // en if som kollar om ormen äter äpplet
+            {
+                Foodx = rand.Next(0, 38) * 15;
+                Foody = rand.Next(0, 20) * 15;
+                score++;
+                scorebord.Text = "Score: " + score.ToString();
+            }
+            else // om ormen inte äter så tar den bort den sista delen av ormen så att den inte växer
+            {
+                if (snare.Count > 0)
+                    snare.RemoveAt(snare.Count - 1);
+            }
+
+            Death();
+            Refresh();
+        }
+
+        private void updbtn_Click(object sender, EventArgs e) // en knapp som stänger ner spelet (bara för roligt)
+        {
+            this.Close();
+        }
+    }
+}
